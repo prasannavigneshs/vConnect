19b0b20aba7a9a67e3b9cbceb3e4e93f09efc1c2
Prasanna Vignesh <prasanna.vigneshs@gmail.com>
2018-12-22 23:04:49 +0530

    Added one more search option
    Added a search option to display class timetables.

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def add_staffs():
         return render_template("staff_signup.html")
     else:
         staff_id = request.form['staffid']
-        name = request.form['name']
+        name = request.form['name'].strip().replace(". ", ".")
         department = request.form['department']
         designation = request.form.get('designation')
         file = request.files['profile_picture']
@@ -51,22 +51,32 @@ def upload_timetable():
         return render_template("upload_timetable.html")
     else:
         department = request.form['department']
-        file = request.files['file']
+        file = request.files['staff_timetable']
         flname = file.filename
         flname = flname.split('.')
-        file.filename = "timetable" + '.' + flname[-1]
+        file.filename = "staff_timetable" + '.' + flname[-1]
 
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        staff_timetable = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], staff_timetable))
 
-        process_timetable(department, filename)
+        file = request.files['class_timetable']
+        flname = file.filename
+        flname = flname.split('.')
+        file.filename = "class_timetable" + '.' + flname[-1]
 
-        return render_template("upload_timetable.html", success=True)
+        class_timetable = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], class_timetable))
+
+        error_msg = process_timetable(department, staff_timetable, class_timetable)
+
+        if error_msg is None:
+            return render_template("upload_timetable.html", success=True)
+        else:
+            return error_msg
 
 
 @app.route('/process-query', methods=['POST'])
 def process_query():
-
     json_data = json.loads(request.form['data'])
     query_type = json_data['type']
 
@@ -103,23 +113,16 @@ def process_query():
 
         return json.dumps({'type': 'result', 'value': details})
 
-    elif query_type == "class":
-
-        cursor.execute("SELECT DISTINCT(staff_id) FROM staff_handling_subjects "
-                       "WHERE class = %s", (json_data['value'][0],))
+    elif query_type == "class_search":
+        cursor.execute("SELECT DISTINCT(department) FROM class_timetable")
         results = cursor.fetchall()
+
         details = []
 
         for result in results:
-            cursor.execute("SELECT * FROM staffs_details WHERE id = %s", (result[0],))
-            query2 = cursor.fetchall()
-            cursor.execute("SELECT period FROM free_periods WHERE staff_id = %s", (result[0],))
-            query3 = cursor.fetchall()
-            json_result = json.loads(query3[0][0])
-            if json_result[json_data['value'][1]][int(json_data['value'][2])] == "":
-                staff = {'id': result[0], 'name': query2[0][1], 'designation': query2[0][2], 'image': query2[0][3]}
-                details.append(staff)
-        return json.dumps({'type': 'class', 'value': details})
+            details.append(result[0])
+
+        return json.dumps({'type': 'result', 'value': details})
 
     elif query_type == "department":
         cursor.execute("SELECT * FROM staffs_details WHERE department = %s", (json_data['value'],))
@@ -140,17 +143,53 @@ def process_query():
         details = []
 
         for ID in ids:
-
             cursor.execute("SELECT * FROM staffs_details WHERE id = %s", (ID,))
             result = cursor.fetchone()
             staff = {'id': result[0], 'name': result[1], 'designation': result[2], 'image': result[3]}
             details.append(staff)
         return json.dumps({'type': 'subjects', 'value': details})
 
+    elif query_type == "class":
+
+        cursor.execute("SELECT DISTINCT(staff_id) FROM staff_handling_subjects "
+                       "WHERE class = %s", (json_data['value'][0],))
+        results = cursor.fetchall()
+        details = []
+
+        for result in results:
+            cursor.execute("SELECT * FROM staffs_details WHERE id = %s", (result[0],))
+            query2 = cursor.fetchall()
+            cursor.execute("SELECT period FROM free_periods WHERE staff_id = %s", (result[0],))
+            query3 = cursor.fetchall()
+            json_result = json.loads(query3[0][0])
+            if json_result[json_data['value'][1]][int(json_data['value'][2])] == "":
+                staff = {'id': result[0], 'name': query2[0][1], 'designation': query2[0][2], 'image': query2[0][3]}
+                details.append(staff)
+        return json.dumps({'type': 'class', 'value': details})
+
+    elif query_type == "sections":
+        cursor.execute("SELECT * FROM class_timetable WHERE department = %s", (json_data['value'],))
+        results = cursor.fetchall()
+
+        cursor.execute("SELECT code FROM departments WHERE department_name = %s", (results[0][0],))
+        result = cursor.fetchall()
+        department_code = result[0][0]
+
+        details = []
+
+        for result in results:
+            details.append({"class": result[1], "id": department_code + "-" + result[1]})
+        print(details)
+        return json.dumps({'type': 'class', 'value': details})
 
 @app.route('/timetable/<arg>')
-def display(arg):
+def staff_timetable(arg):
     return render_template("timetable.html", id=arg)
+
+
+@app.route('/class-timetable/<string:arg>')
+def class_timetable(arg):
+    return render_template("class_timetable.html", id=arg)
 
 
 @app.route('/load-from-db', methods=["POST"])
@@ -158,25 +197,37 @@ def load_from_db():
     mysql_con = mysql.connector.connect(host="localhost", user="root", passwd="", database="vconnect")
 
     cursor = mysql_con.cursor()
+    if request.form['type'] == "staff":
+        cursor.execute("SELECT period FROM free_periods WHERE staff_id = %s", (request.form['data'],))
 
-    cursor.execute("SELECT period FROM free_periods WHERE staff_id = %s", (request.form['data'],))
+        result1 = cursor.fetchall()
 
-    result1 = cursor.fetchall()
+        cursor.execute("SELECT subject_name, class FROM staff_handling_subjects "
+                       "WHERE staff_id = %s", (request.form['data'],))
 
-    cursor.execute("SELECT subject_name, class FROM staff_handling_subjects "
-                   "WHERE staff_id = %s", (request.form['data'],))
+        result2 = cursor.fetchall()
 
-    result2 = cursor.fetchall()
+        cursor.execute("SELECT staff_name FROM staffs_details WHERE id = %s", (request.form['data'],))
 
-    cursor.execute("SELECT staff_name FROM staffs_details WHERE id = %s", (request.form['data'],))
+        result3 = cursor.fetchall()
 
-    result3 = cursor.fetchall()
+        return json.dumps({"value": {"timetable": result1, "subjects": result2, "name": result3}})
+    else:
+        data, ind= request.form['data'], request.form['data'].index('-')
+        department_code, class_sec = data[0:ind], data[ind + 1: ]
 
-    return json.dumps({"value": {"timetable": result1, "subjects": result2, "name": result3}})
+        cursor.execute("SELECT department_name FROM departments WHERE code = %s", (department_code,))
+        result = cursor.fetchall()
+
+        department = result[0][0]
+
+        cursor.execute("SELECT * FROM class_timetable WHERE department = %s AND class = %s", (department, class_sec))
+        result = cursor.fetchall()
+
+        return json.dumps({"value": {"timetable": result[0][3], "details": [result[0][1], result[0][2]], "subject_details": result[0][4]}})
 
 
-def process_timetable(department, filename):
-    book = xlrd.open_workbook(UPLOAD_FOLDER + filename)
+def process_timetable(department, staff_timetable, class_timetable):
 
     db = mysql.connector.connect(host="localhost", user="root", passwd="", database="vconnect")
     cursor = db.cursor()
@@ -185,8 +236,11 @@ def process_timetable(department, filename):
                    "(SELECT id FROM staffs_details WHERE department = %s)", (department,))
     cursor.execute("DELETE FROM free_periods WHERE staff_id IN "
                    "(SELECT id FROM staffs_details WHERE department = %s)", (department,))
+    cursor.execute("DELETE FROM class_timetable WHERE department = %s", (department,))
 
     db.commit()
+
+    book = xlrd.open_workbook(UPLOAD_FOLDER + staff_timetable)
 
     cnt = book.nsheets
     names = book.sheet_names()
@@ -197,7 +251,6 @@ def process_timetable(department, filename):
         if sheet_cnt == sa2:
             continue
         sheet = book.sheet_by_index(sheet_cnt)
-        print(sheet.name)
         ind = 0
         for cell in range(6, 15):
             if sheet.cell_value(cell, 1) == "DAY":
@@ -239,8 +292,13 @@ def process_timetable(department, filename):
                     ind_diff = 5
                 periods[rlo - ind][clo + 1 - ind_diff] = periods[rlo - ind][clo - ind_diff]
 
-        cursor.execute("INSERT INTO free_periods VALUES (%s,%s)", (names[sheet_cnt], json.dumps(periods)))
-        db.commit()
+        cursor.execute("SELECT * FROM staffs_details WHERE id = %s",(names[sheet_cnt],))
+        result = cursor.fetchall()
+        if len(result) != 0:
+            cursor.execute("INSERT INTO free_periods VALUES (%s,%s)", (names[sheet_cnt], json.dumps(periods)))
+            db.commit()
+        else:
+            return "Error in sheet name - " + names[sheet_cnt]
 
     sheet = book.sheet_by_index(sa2)
     name = ""
@@ -248,23 +306,104 @@ def process_timetable(department, filename):
     for cell in range(5, sheet.nrows):
 
         if sheet.cell_value(cell, 1) != "":
-            name = sheet.cell_value(cell, 1).strip()
-        print(name)
+            name = sheet.cell_value(cell, 1).strip().replace(". ", ".")
         cursor.execute("SELECT id FROM staffs_details WHERE staff_name = %s", (name,))
         result = cursor.fetchall()
-        row_id = result[0][0]
+        try:
+            row_id = result[0][0]
 
-        if sheet.cell_value(cell, 4) != "":
-            cursor.execute("INSERT INTO staff_handling_subjects (staff_id,subject_name,class) VALUES (%s,%s,%s)",
-                           (row_id, sheet.cell_value(cell, 2),
-                            sheet.cell_value(cell, 4) + " " + sheet.cell_value(cell, 3)))
-            db.commit()
+            if sheet.cell_value(cell, 4) != "":
+                cursor.execute("INSERT INTO staff_handling_subjects (staff_id,subject_name,class) VALUES (%s,%s,%s)",
+                               (row_id, sheet.cell_value(cell, 2),
+                                sheet.cell_value(cell, 4) + " " + sheet.cell_value(cell, 3)))
+                db.commit()
 
-        if sheet.cell_value(cell, 6) != "" and "NA" not in sheet.cell_value(cell, 6):
-            cursor.execute("INSERT INTO staff_handling_subjects (staff_id,subject_name,class) VALUES (%s,%s,%s)",
-                           (row_id, sheet.cell_value(cell, 6),
-                            sheet.cell_value(cell, 8) + " " + sheet.cell_value(cell, 7)))
-            db.commit()
+            if sheet.cell_value(cell, 6) != "" and "NA" not in sheet.cell_value(cell, 6):
+                cursor.execute("INSERT INTO staff_handling_subjects (staff_id,subject_name,class) VALUES (%s,%s,%s)",
+                               (row_id, sheet.cell_value(cell, 6),
+                                sheet.cell_value(cell, 8) + " " + sheet.cell_value(cell, 7)))
+                db.commit()
+        except:
+            return "Error in the name of staff - " + name
+
+    book = xlrd.open_workbook(UPLOAD_FOLDER + class_timetable)
+
+    cnt = book.nsheets
+    names = book.sheet_names()
+
+
+    for sheet_cnt in range(cnt):
+
+        sheet = book.sheet_by_index(sheet_cnt)
+        ind, advisor_ind, subjects = 0, 0, 0
+        for cell in range(4, 20):
+            if "CLASS ADVISOR" in sheet.cell_value(cell, 1) or "CLASS ADVISOR" in sheet.cell_value(cell, 2):
+                advisor_ind = cell
+            elif sheet.cell_value(cell, 1) == "DAY":
+                ind = cell + 1
+            elif sheet.cell_value(cell, 1) == "SUB CODE":
+                subjects = cell + 1
+                break
+
+        periods = {}
+
+        advisor_name = ""
+        for cell in range(2, 20):
+            if sheet.cell_value(advisor_ind, cell) != "" and "CLASS ADVISOR" not in sheet.cell_value(advisor_ind, cell):
+                advisor_name = sheet.cell_value(advisor_ind, cell).strip()
+                break
+
+        for cell in range(5):
+            day = []
+            counter = 2
+
+            while counter < 13:
+                if sheet.cell_value(cell + ind, counter) != "":
+                    day.append(sheet.cell_value(cell + ind, counter))
+
+                elif counter == 4 or counter == 7 or counter == 10:
+                    counter += 1
+                    continue
+
+                else:
+                    day.append("")
+
+                counter += 1
+
+            periods[cell] = day
+
+        for crange in sheet.merged_cells:
+            rlo, rhi, clo, chi = crange
+            if rlo >= ind and rlo < (ind + 5):
+                ind_diff = 0
+                if clo < 4:
+                    ind_diff = 2
+                elif clo >= 4 and clo < 7:
+                    ind_diff = 3
+                elif clo >= 7 and clo < 10:
+                    ind_diff = 4
+                else:
+                    ind_diff = 5
+                periods[rlo - ind][clo + 1 - ind_diff] = periods[rlo - ind][clo - ind_diff]
+
+        subject_collection = []
+
+        for row in range(subjects, subjects + 10):
+            code, subject, faculty = "", "", ""
+            if sheet.cell_value(row, 1) == "":
+                break
+            for col in range(1, 20):
+                if sheet.cell_value(row, col) != "":
+                    if code == "":
+                        code = sheet.cell_value(row, col)
+                    elif subject == "":
+                        subject = sheet.cell_value(row, col)
+                    elif faculty == "":
+                        faculty = sheet.cell_value(row, col)
+            subject_collection.append({"code": code, "subject": subject, "faculty": faculty})
+
+        cursor.execute("INSERT INTO class_timetable VALUES(%s, %s, %s, %s, %s)" ,(department, str(sheet.name).upper(), advisor_name, json.dumps(periods), json.dumps(subject_collection)))
+        db.commit()
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,27 @@
 var flag = true;
 
 function validateFileExtension() {
-    var filename = document.getElementById('file').value;
+    var filename = document.getElementById('staff_timetable').value;
     var extension = filename.substring(filename.lastIndexOf('.') + 1);
 
     if(extension === "xls" || extension === "xlsx") {
-        return true;
+
+        filename = document.getElementById('class_timetable').value;
+        extension = filename.substring(filename.lastIndexOf('.') + 1);
+
+        if(extension === "xls" || extension === "xlsx") {
+            document.getElementById('uploading').style.display = "block";
+            return true;
+        }
+        else {
+            element = document.getElementById('feedback_class').style;
+            element.display = "";
+            element.color = "red";
+            return false;
+        }
     }
     else {
-        element = document.getElementById('feedback').style;
+        element = document.getElementById('feedback_staff').style;
         element.display = "";
         element.color = "red";
         return false;
@@ -44,7 +57,7 @@ function getFields(value) {
         dataList.innerHTML ="";
     }
     var data = {};
-    fields = ["department_search","subject_search","free_period"]
+    fields = ["department_search","subject_search","free_period","class_search"]
     data["type"]=fields[parseInt(doc)];
 
     request.onreadystatechange = function(response) {
@@ -86,7 +99,7 @@ function getResults(field1, field2) {
         }
     }
     var data = {};
-    fields = ["department","subject","class"]
+    fields = ["department", "subject", "class", "sections"]
     data["type"]=fields[parseInt(doc)];
     if(data["type"] === "class") {
         if(field1 == undefined) {
@@ -113,7 +126,7 @@ function getResults(field1, field2) {
                 getFields();
                 document.getElementById('search_bar').value = document.getElementById('searchbar').value;
           }
-          fillProfiles(jsonOptions['value']);
+          fillProfiles(jsonOptions['value'], data["type"]);
         }
       }
     };
@@ -125,28 +138,46 @@ function getResults(field1, field2) {
 
 }
 
-function fillProfiles(data) {
+function fillProfiles(data, type) {
     var text = "";
-    for(var i = 0; i < data.length; i ++ ) {
-        text += '<div class="staff-detail col-xd-3 col-sm-3 col-md-3">' +
-        '<center>'+
-            '<a href="/timetable/' + data[i]['id'] + '" target="_blank">'+
-                '<div class = "profile_picture">'+
-                    '<img  class="rounded-circle " src="/static/uploads/' + getImageName(data[i]['name']) + '.jpg" alt="image" height="200" width="200">'+
+    if(type !== "sections") {
+        for(var i = 0; i < data.length; i ++ ) {
+            text += '<div class="staff-detail col-xd-3 col-sm-3 col-md-3">' +
+            '<center>'+
+                '<a href="/timetable/' + data[i]['id'] + '" target="_blank">'+
+                    '<div class = "profile_picture">'+
+                        '<img  class="rounded-circle " src="/static/uploads/' + getImageName(data[i]['name']) + '.jpg" alt="image" height="200" width="200">'+
+                    '</div>'+
+                '</a>'+
+                '<div class="details">'+
+                    '<h5> <p class="name">' +  data[i]['name'] + '</p></h5>'+
+                    '<p class="designation">' + data[i]['designation'] + '</p>'+
+                    '<a href="/timetable/' + data[i]['id'] + '" type="button" class="btn btn-info" target="_blank">View Timetable</button>'+
                 '</div>'+
-            '</a>'+
-            '<div class="details">'+
-                '<h5> <p class="name">' +  data[i]['name'] + '</p></h5>'+
-               '<p class="designation">' + data[i]['designation'] + '</p>'+
-                '<a href="/timetable/' + data[i]['id'] + '" type="button" class="btn btn-info" target="_blank">View Timetable</button>'+
-            '</div>'+
-        '</center>'+
-    '</div>';
+            '</center>'+
+        '</div>';
+        }
+    } else {
+        for(var i = 0; i < data.length; i ++ ) {
+            text += '<div class="staff-detail col-xd-3 col-sm-3 col-md-3">' +
+            '<center>'+
+                '<a href="/class-timetable/' + data[i]['id'] + '" target="_blank">'+
+                    '<div class = "profile_picture">'+
+                        '<img  class="rounded-circle " src="/static/uploads/' + getImageName(data[i]['class']) + '.jpg" alt="image" height="200" width="200">'+
+                    '</div>'+
+                '</a>'+
+                '<div class="details">'+
+                    '<h5> <p class="name">' +  data[i]['class'] + '</p></h5>'+
+                    '<a href="/class-timetable/' + data[i]['id'] + '" type="button" class="btn btn-info" target="_blank">View Timetable</button>'+
+                '</div>'+
+            '</center>'+
+        '</div>';
+        }
     }
     document.getElementById('profiles').innerHTML = text;
 }
 
-function loadValueFromDb(id) {
+function loadStaffTimetable(id) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if(this.readyState ===4 && this.status === 200) {
@@ -189,7 +220,82 @@ function loadValueFromDb(id) {
     xhttp.open("POST", "/load-from-db", false);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     try {
-        xhttp.send("data="+id);
+        xhttp.send("type=staff&data="+id);
+    } catch(err) {
+        alert("Oops send");
+    }
+
+}
+
+function loadClassTimetable(id) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState ===4 && this.status === 200) {
+            var json = JSON.parse(this.responseText);
+            console.log(JSON.parse(json['value']['subject_details']));
+            var result = JSON.parse(json['value']['timetable']);
+            details = json['value']['details'];
+            detail = '<center>' +
+                        '<table class="table" style="margin-top:3%;width:40%">' +
+                            '<tr>' +
+                                '<th class="col-md-1" style="width:50%" > Class </th>' +
+                                '<td class="col-md-1">' + json['value']['details'][0] + '</td>' +
+                            '</tr>' +
+                            '<tr>' +
+                                '<th class="col-md-1" style="width:50%"> Class Advisor </th>' +
+                                '<td class="col-md-1">' + json['value']['details'][1] + '</td>' +
+                            '</tr>' +
+                        '</table>' +
+                      '</center>';
+            document.getElementById('details').innerHTML = detail;
+
+            day = ["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY"]
+            text = "<table border='2'>";
+            for(var i = 0 ;i < 5; i++)
+            {
+                text+='<tr>';
+                text+= '<th scope="row">'+day[i]+'</th>';
+                for(var j= 0 ; j < 8; j++) {
+                        if(result[i][j]==="")
+                            text+='<td>     </td>';
+                        else if ((j%2 == 0) && result[i][j] === result[i][j + 1]) {
+                            text+='<td colspan="2" align="center">'+result[i][j]+'</td>';
+                             j++;
+                        }
+                        else
+                            text+='<td align="center">'+result[i][j]+'</td>';
+                }
+
+                text+='</tr>';
+            }
+            text+='</table>';
+            document.getElementById('rows').innerHTML = text;
+
+            subject_details = JSON.parse(json['value']['subject_details'])
+            subject_detail = '<center>' +
+                        '<table class="table" style="width:60%">' +
+                            '<tr>' +
+                                '<th class="col-md-3" style="width:20%"> Subject Code </th>' +
+                                '<th class="col-md-3"> Subject Name </th>' +
+                                '<th class="col-md-3"> Faculty </th>' +
+                            '</tr>' ;
+            for(var i = 0; i < subject_details.length; i ++) {
+                subject_detail += '<tr>' +
+                        '<td class="col-md-3" style="width:20%">' + subject_details[i]["code"] + '</th>' +
+                        '<td class="col-md-3">' + subject_details[i]["subject"] + '</th>' +
+                        '<td class="col-md-3">' + subject_details[i]["faculty"] + '</th>' +
+                    '</tr>';
+            }
+            subject_detail += '</table></center>';
+
+            document.getElementById('subject_details').innerHTML = subject_detail;
+        }
+    };
+
+    xhttp.open("POST", "/load-from-db", false);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    try {
+        xhttp.send("type=class&data="+id);
     } catch(err) {
         alert("Oops send");
     }
@@ -201,5 +307,8 @@ function getImageName(name) {
         return "doc";
     else if(name.includes("Mrs") || name.includes("Ms"))
         return "female";
-    return "male";
+    else if(name.includes("Mr"))
+        return "male";
+    else
+        return "classroom";
 }
